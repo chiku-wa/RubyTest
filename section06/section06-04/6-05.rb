0506ce9fbc359a78ed42c04674df4b2a03029f60
watson <chikuwa.wachi@gmail.com>
2019-03-18 09:28:54 +0900

    [change]Completed section 6

--- section06/section06-04/6-05.rb
+++ section06/section06-04/6-05.rb
@@ -59,3 +59,108 @@ def section_06_05_03
 end
 
 section_06_05_03
+
+# ===
+# === 6.5.4 組み込み変数でマッチの結果を取得する
+def section_06_05_04
+  # Rubyでは「$」と呼ばれる組み込み変数が存在し、「=~」や「match」の実行結果が格納される
+  # [復習]
+  # =~ マッチした文字列の位置を返す
+  # match マッチした文字列をMatchDataオブジェクトとして返す
+  birthday = "私の誕生日は1980年5月30日です"
+
+  # 比較を行う
+  birthday =~ /(\d{4})年(\d{1,2})月(\d{1,2})日/
+
+  # 「$~」は、MatchDataオブジェクトを取得する
+  p $~ #=> #<MatchData "1980年5月24日" 1:"1980" 2:"5" 3:"30">
+
+  # 「$&」は、マッチした部分をすべて取得する
+  p $& #=> 1980年5月30日
+
+  # 1〜3番目のキャプチャを取得する
+  p $1 #=> 1980
+  p $2 #=> 5
+  p $3 #=> 30
+
+  # 最後のキャプチャを取得する
+  p $+  #=> 30
+
+  # とはいえ、可読性の意味でもmatchメソッドの実行結果をそのまま用いるほうが可読性が高い
+  res = birthday.match(/(\d{4})年(\d{1,2})月(\d{1,2})日/)
+
+  # $~ と同じ意味
+  p res
+
+  # $& と同じ意味
+  p res[0] #=> 1980年5月30日
+  # res.to_sでも同様
+
+  # $n と同じ意味
+  p res[1] #=> 1980
+  p res[2] #=> 5
+  p res[3] #=> 30
+
+  # $+ と同じ意味
+  p res[-1] #=> 30
+end
+
+section_06_05_04
+
+# ===
+# === 6.5.5 Regexp.last_matchでマッチの結果を取得する
+def section_06_05_05
+  # Regexp.last_matchを使うと、先ほどの組み込み変数と同様に、最後に実行した「=~」やmatch
+  # メソッドの結果を返す
+  text = "私の誕生日は2000年1月23日、住居の郵便番号123-4567"
+  regex_zip_code = /(\d{3})-(\d{4})/
+  regex_ymd = /(\d{4})年(\d{1,2})月(\d{1,2})日/
+
+  text =~ regex_zip_code
+  text =~ regex_ymd # こちらの結果がRegexp.last_matchに格納される
+
+  # $~ と同じ意味
+  p Regexp.last_match #=> #<MatchData "2000年1月23日" 1:"2000" 2:"1" 3:"23">
+
+  # $& と同じ意味
+  p Regexp.last_match(0) #=> 2000年1月23日
+
+  # $n と同じ意味
+  p Regexp.last_match(1) #=> 2000
+  p Regexp.last_match(2) #=> 1
+  p Regexp.last_match(3) #=> 23
+
+  # $+ と同じ意味
+  p Regexp.last_match(-1) #=> 23
+end
+
+section_06_05_05
+
+# ===
+# === 6.5.6 組み込み変数を書き換えないmatch?メソッド
+def section_06_05_06
+  # Ruby2.4で追加されたmatch?メソッド(一致したかをtrue,falseで返す)では、組み込み変数や
+  # Regexp.last_matchの結果を書き換えない
+  text = "私の誕生日は2003年4月30日、住居の郵便番号999-8888"
+
+  regex_zip_code = /(\d{3})-(\d{4})/
+  regex_ymd = /(\d{4})年(\d{1,2})月(\d{1,2})日/
+
+  text.match(regex_zip_code)
+  text.match?(regex_ymd) #=> ←の結果は組み込み変数や、Revexp.last_matchに格納されない
+
+  p $& #=> 999-8888
+  p Regexp.last_match(0) #=> 999-8888
+
+  # この他にも、RegexpクラスやMatchDataクラスには多くの機能が用意されているため、
+  # 下記のドキュメントを参照すること
+
+  # Regexpクラス
+  # https://docs.ruby-lang.org/ja/latest/class/Regexp.html
+
+  # MatchDataクラス(結果を格納するクラス)
+  # https://docs.ruby-lang.org/ja/latest/class/MatchData.html
+
+end
+
+section_06_05_06
