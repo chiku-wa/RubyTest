fd984c3e206e6ee678dc28a4724027928c2633c1
watson <chikuwa.wachi@gmail.com>
2019-03-23 13:17:06 +0900

    [change]Completed until section7.4 and  7.5.1

--- section07/section07-04/test/gate_test.rb
+++ section07/section07-04/test/gate_test.rb
@@ -5,25 +5,47 @@ require_relative "../lib/gate.rb"
 require_relative "../lib/ticket.rb"
 
 class GateTest < MiniTest::Test
-  def test_gate
+  # setupメソッドは、各テストメソッドの前にかならず実行されるため、Gateオブジェクト生成
+  # はこのメソッドにまとめてしまう
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
+  def test_gate_umeda_to_juso
     # 梅田で券を発行して入場する
-    umeda = Gate.new(:umeda)
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
+    @umeda.enter(ticket)
 
     # 発行したチケットで十三で出場する
-    juso = Gate.new(:juso)
-    assert juso.exit(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_gate_umeda_to_mikuni_when_fare_is_not_enough
     # 梅田で券を発行して入場する
-    umeda = Gate.new(:umeda)
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
+    @umeda.enter(ticket)
 
     # 発行したチケットで三国で出場する
-    mikuni = Gate.new(:mikuni)
-    refute mikuni.exit(ticket) # refute → 結果がfalseであればテストをパスする
+    refute @mikuni.exit(ticket) # refute → 結果がfalseであればテストをパスする
+  end
+
+  def test_gate_umeda_to_mikuni_when_fare_is_enough
+    # 梅田で券を発行して入場する
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+
+    # 三国で出場する
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_gate_juso_to_mikuni
+    # 十三で券を発行して入場する
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+
+    # 三国で出場する
+    assert @mikuni.exit(ticket)
   end
 end
