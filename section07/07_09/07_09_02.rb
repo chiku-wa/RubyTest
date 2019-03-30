91163f88fd6f19d4538ed412ae856bde7c92752b
watson <chikuwa.wachi@gmail.com>
2019-03-30 13:28:21 +0900

    [change]Fixed section 7.9.2.

--- section07/07_09/07_09_02.rb
+++ section07/07_09/07_09_02.rb
@@ -26,28 +26,29 @@ class DVD < Product
 
   # スーパークラスと同じinitializeメソッドなら、メソッド自体を省略できる
 
-  def name_upcase
-    @@name.upcase
+  def name_upcase!
+    @@name.upcase!
   end
 end
 
 # クラス直下で定義されている変数を呼び出し
-# クラスを呼び出した時点で「Product」で初期化される
-puts Product.name #=> Product
+# DVDクラスを【定義】した時点で共通変数である@@nameが書き換わってしまう
+puts Product.name #=> DVD ※Productではない
 puts DVD.name #=> DVD
 
 # インスタンスメソッドで@@nameを書き換える
 product = Product.new("Tom Clancy")
 puts product.name #=> Tom Clancy
 
-# クラスから直接呼び出すと@@nameが書き換わっていることを確認できる
-puts Product.name #=> Toy Story
-# スーパークラスの変更に連動して、サブクラスの@@nameも書き換わっている
-puts DVD.name #=> Toy Story
+# クラスから直接呼び出しても書き換わっている
+puts Product.name #=> Tom Clancy
+# サブクラスの@@nameも書き換わっている
+puts DVD.name #=> Tom Clancy
 
-# # サブクラスの変更に連動して、スーパークラスの@@name変数も書き換わっている
-# dvd = DVD.new("Toy Story")
-# puts dvd.name #=>Toy Story
+# サブクラスの変更に連動して、スーパークラスの@@name変数も書き換わっている
+dvd = DVD.new("Toy Story")
+puts dvd.name #=>Toy Story
 
-# dvd.name_upcase
-# puts Product.name
+# インスタンスメソッドで破壊的にクラス変数を書き換える
+dvd.name_upcase!
+puts Product.name #=> TOY STORY
